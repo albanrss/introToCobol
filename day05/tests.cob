d060d40e2f42aa782750df7654784a1e7411c343
albanrss <alban.roussee@epitech.eu>
2026-01-22 11:00:03 +0100

    feat(day05): add tests for myComputePowerIt program

--- day05/tests.cob
+++ day05/tests.cob
@@ -6,6 +6,8 @@
        working-storage section.
        01  factIt pic s9(9) value 4.
        01  factRec pic s9(9) value 4.
+       01  nbPowerIt pic s9(9) value 4.
+       01  pPowerIt pic s9(9) value 4.
 
        procedure division.
        display "myComputeFactorialIt " factIt.
@@ -14,6 +16,10 @@
 
        display "myComputeFactorialRec " factRec.
        call "myComputeFactorialRec" using factRec.
+       display return-code.
+
+       display "myComputePowerIt " nbPowerIt " " pPowerIt.
+       call "myComputePowerIt" using nbPowerIt pPowerIt.
        display return-code.
 
        stop run.
