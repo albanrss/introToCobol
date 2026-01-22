f36854b3b24ce46cb57e84895534830f6e58fbc5
albanrss <alban.roussee@epitech.eu>
2026-01-22 10:54:29 +0100

    feat(day05): add myComputeFactorialRec program to compute factorial recursively

---
+++ day05/myComputeFactorialRec.cob
@@ -0,0 +1,26 @@
+       identification division.
+       program-id. myComputeFactorialRec is recursive.
+
+
+       data division.
+       local-storage section.
+       01  inputNb pic s9(9).
+
+       linkage section.
+       01  nb pic s9(9).
+
+
+       procedure division using nb.
+       if nb is negative
+           move 0 to return-code
+       end-if.
+       if nb = 0 or nb = 1
+           move 1 to return-code
+       else
+           subtract 1 from nb giving inputNb
+           call "myComputeFactorialRec" using inputNb
+           multiply return-code by nb giving return-code
+       end-if.
+       exit program.
+
+       end program myComputeFactorialRec.
