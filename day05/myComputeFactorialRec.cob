e707165538f337d0c88b0c8203887057172614ed
albanrss <alban.roussee@epitech.eu>
2026-01-22 11:04:42 +0100

    fix(day05): correct control flow in myComputeFactorialRec program

--- day05/myComputeFactorialRec.cob
+++ day05/myComputeFactorialRec.cob
@@ -13,13 +13,14 @@
        procedure division using nb.
        if nb is negative
            move 0 to return-code
-       end-if.
-       if nb = 0 or nb = 1
-           move 1 to return-code
        else
-           subtract 1 from nb giving inputNb
-           call "myComputeFactorialRec" using inputNb
-           multiply return-code by nb giving return-code
+           if nb = 0 or nb = 1
+               move 1 to return-code
+           else
+               subtract 1 from nb giving inputNb
+               call "myComputeFactorialRec" using inputNb
+               multiply return-code by nb giving return-code
+           end-if
        end-if.
        exit program.
 
