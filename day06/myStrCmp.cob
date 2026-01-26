edc14e4c70fb8264d652677f53b9f0eeeca28c6f
albanrss <alban.roussee@epitech.eu>
2026-01-26 13:38:52 +0100

    feat(day06): add myStrCmp program for string comparison functionality

---
+++ day06/myStrCmp.cob
@@ -0,0 +1,28 @@
+       identification division.
+       program-id. myStrCmp.
+
+
+       data division.
+       working-storage section.
+       01  idx pic 9(9).
+
+       linkage section.
+       01  str1 pic x any length.
+       01  str2 pic x any length.
+
+
+       procedure division using str1 str2.
+       move 0 to return-code.
+       perform varying idx from 0
+           until idx > function length(str1)
+           or idx > function length(str2)
+           if function ord(str1(idx:1)) not = function ord(str2(idx:1))
+               subtract function ord(str1(idx:1))
+                   from function ord(str2(idx:1))
+                   giving return-code
+               exit perform
+           end-if
+       end-perform.
+
+
+       end program myStrCmp.
