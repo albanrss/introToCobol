bbc57cae3a8c0e4c4254e7c5db9a797cdfef855a
albanrss <alban.roussee@epitech.eu>
2026-01-27 12:06:11 +0100

    fix(day06): refactor myStrCmp to use temporary variables for character comparison

--- day06/myStrCmp.cob
+++ day06/myStrCmp.cob
@@ -5,6 +5,8 @@
        data division.
        working-storage section.
        01  idx pic 9(9).
+       01  c1 pic 9(9).
+       01  c2 pic 9(9).
 
        linkage section.
        01  str1 pic x any length.
@@ -17,12 +19,20 @@
            until idx > function length(str1)
            or idx > function length(str2)
            if function ord(str1(idx:1)) not = function ord(str2(idx:1))
-               subtract function ord(str1(idx:1))
-                   from function ord(str2(idx:1))
-                   giving return-code
                exit perform
            end-if
        end-perform.
 
+       move function ord(str1(idx:1)) to c1.
+       move function ord(str2(idx:1)) to c2.
+
+       if idx > function length(str1)
+           move 0 to c1
+       end-if.
+       if idx > function length(str2)
+           move 0 to c2
+       end-if.
+
+       subtract c2 from c1 giving return-code.
 
        end program myStrCmp.
