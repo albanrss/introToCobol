4f4edfa6eb7fe846b6b9b4f19fdd256f8e6cddd9
albanrss <alban.roussee@epitech.eu>
2026-01-27 14:29:04 +0100

    fix(day06): format myStrUpCase character conversion for better readability

--- day06/myStrUpCase.cob
+++ day06/myStrUpCase.cob
@@ -13,8 +13,10 @@
        procedure division using str.
        perform varying idx from 1 until idx > function length(str)
            if 'a' <= str(idx:1) and str(idx:1) <= 'z'
-               move function char(function ord(str(idx:1))
-                   + function ord('A') - function ord('a'))
+               move function char(
+                   function ord(str(idx:1))
+                   + (function ord('A') - function ord('a'))
+                   )
                    to str(idx:1)
            end-if
        end-perform.
