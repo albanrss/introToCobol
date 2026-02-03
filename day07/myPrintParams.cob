03ae1eda158a58dacda814fc6e1ce2a720cf1a4a
albanrss <alban.roussee@epitech.eu>
2026-02-03 13:53:36 +0100

    feat(day07): refactor myPrintParams to process and display command-line arguments

--- day07/myPrintParams.cob
+++ day07/myPrintParams.cob
@@ -4,12 +4,26 @@
 
        data division.
        working-storage section.
-       01  commandLineData pic x(256).
+       78  MAX_LENGTH value 128.
+       01  commandLineData pic x(MAX_LENGTH).
+       01  currentArg pic x(MAX_LENGTH).
+       01  ptr pic 9(9) value 0.
+       01  lastPtr pic 9(9).
 
 
        procedure division.
        accept commandLineData from command-line.
-       display commandLineData.
+       inspect commandLineData tallying ptr for leading spaces
+       add 1 to ptr.
+       perform until ptr equals lastPtr
+           move ptr to lastPtr
+           unstring commandLineData delimited by all space
+               into currentArg
+               pointer ptr
+           if ptr not equals lastPtr
+               display currentArg
+           end-if
+       end-perform.
 
 
        end program myPrintParams.
